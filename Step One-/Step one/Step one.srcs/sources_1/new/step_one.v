a2f068102e247255b8a222eb1db09158861999e1
rokanvVR <rokanv0806@gmail.com>
2025-10-15 21:39:05 +0530

    Add files via upload

---
+++ Step One-/Step one/Step one.srcs/sources_1/new/step_one.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.10.2025 20:41:35
+// Design Name: 
+// Module Name: step_one
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module step_one(
+    output one
+    );
+    assign one = 1;
+endmodule
