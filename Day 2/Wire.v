600e31ca766219b5118b7dcca161d6f0e48ee171
rokanvVR <rokanv0806@gmail.com>
2025-10-18 14:10:02 +0530

    Add files via upload

---
+++ Day 2/Wire.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.10.2025 21:08:58
+// Design Name: 
+// Module Name: Wire
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Wire(
+    input in,
+    output out
+    );
+    assign out = in;
+endmodule
---
+++ Day 2/wires4.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.10.2025 21:18:27
+// Design Name: 
+// Module Name: wires4
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module wires4(
+    input a,b,c,
+    input w,x,y,z
+    );
+    assign w =a;
+    assign x = b;
+    assign y = b;
+    assign z = c;
+endmodule
